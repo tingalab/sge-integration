3c447d7ab87d38fc42fb34976def3804a61950a3
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-08-12 16:16:06 -0400

    updated plotting function
    Added optional code to print larger fonts

--- scripts/SPOTlight.R
+++ scripts/SPOTlight.R
@@ -42,4 +42,32 @@ ggsave(plot =  patchwork::wrap_plots(purrr::map(cell.names, function(x) SPOTligh
      pie_scale = 0.4)), ncol = 4), filename="figures/SPOTlight/Figure_3b.pdf", 
     device="pdf", width = 25, height = 25, units = "in", dpi = 300)
 
+# OPTIONAL: Added another version of plotting function to print larger fonts in the figures
+                                     
+cell.names <- names(which(colSums(U1.SL@meta.data[,-c(1:4, ncol(U1.SL@meta.data))]) > 0))
+scplots<-purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U1.SL,  cell_types_all = cell.names,  cell_types_interest=x,  
+img_path = "data/U1/spatial/tissue_lowres_image.png",
+pie_scale = 0.4)+
+ theme(title = element_text(size=20),
+       legend.text = element_text(size = 18),
+       legend.title = element_text(size = 18),
+       axis.title = element_text(size=18),
+       axis.title.y = element_text(angle = 90),
+       axis.text = element_text(size=18)))
+
+patchwork::wrap_plots(scplots, ncol=3) %T>% ggsave(filename="figures/SPOTlight/Figure3a.pdf", device="pdf", width = 25, height = 30, units = "in", dpi = 300)
+
+
+cell.names <- names(which(colSums(U2.SL@meta.data[,-c(1:4, ncol(U2.SL@meta.data))]) > 0))
+scplots<-purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U2.SL,  cell_types_all = cell.names,  cell_types_interest=x,  
+img_path = "data/U2/spatial/tissue_lowres_image.png",
+pie_scale = 0.4)+
+ theme(title = element_text(size=20),
+       legend.text = element_text(size = 18),
+       legend.title = element_text(size = 18),
+       axis.title = element_text(size=18),
+       axis.title.y = element_text(angle = 90),
+       axis.text = element_text(size=18)))
+
+patchwork::wrap_plots(scplots, ncol=3) %T>% ggsave(filename="figures/SPOTlight/Figure3b.pdf", device="pdf", width = 25, height = 30, units = "in", dpi = 300)
 
