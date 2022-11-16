0f18e3665dd559eaae6f1559c8418e5a7b7b3fb0
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-11-15 22:32:17 -0500

    added more detailed instructions as comments

--- scripts/setup.R
+++ scripts/setup.R
@@ -9,17 +9,26 @@ library(patchwork)
 library(stringr)
 library(R.utils)
 
-# Set this to your sge-integration folder
-setwd("/home/bradlem4/sge-integration/")
+# Set sge-integration folder as the currect working directory
+
+homedir<-"/home/bradlem4/" # replace the path within quotes to your current/home directory where you downloaded the sge-integration repository
+setwd(paste0(homedir,"/sge-integration/"))
 
 # Download Visium data from GitHub release
 download.file(url = "https://github.com/tingalab/sge-integration/releases/download/V1/protocol-data.tar.gz", destfile = "protocol-data.tar.gz")
 gunzip("protocol-data.tar.gz")
-# file.remove("protocol-data.tar.gz")
+untar("protocol-data.tar")
+file.remove("protocol-data.tar")
+
+# The above lines will download protocol data packaged within "data" named folder to your sge-integration folder #
+# This "data" directory harbors subdirectories "U1" and "U2" that contain Visium spatial transcriptomics data for 2 ureter samples
+# Another sudirectory "scRNA" contains single cell RNA-seq metadata from our Ureter manuscript (Fink and Sona et al., 2022)
+# The matching single cell RNA-seq data can be downloaded to this "scRNA" subdirectory using the following code:
 
 # Download single-cell data from GEO
 download.file(url = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE184nnn/GSE184111/suppl/GSE184111_scRNA_ureter10_normalized_counts.txt.gz", destfile = "data/scRNA/count-matrix.txt.gz")
 gunzip("data/scRNA/count-matrix.txt.gz")
+
 scRNA <- read.csv(file = "data/scRNA/count-matrix.txt", sep = "\t")
 colnames(scRNA) %<>% str_replace_all(., pattern = "\\.", replacement="-")
 
