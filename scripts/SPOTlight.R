276514192904acec12789ed19c63e27b80931841
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-11-30 14:28:24 -0500

    Update SPOTlight.R

--- scripts/SPOTlight.R
+++ scripts/SPOTlight.R
@@ -7,16 +7,17 @@
   library(igraph)
   library(RColorBrewer)
   
-  # Set this to your sge-integration folder
-  setwd("/home/bradlem4/sge-integration/")
+# Set this to your sge-integration folder
+mydir='/home/sonas/star_protocol/'
+
+setwd(paste0(mydir,"sge-integration/"))
   
-  source("scripts/functions.R")
-  scRNA <- readRDS("data/scRNA/ureter-scRNA.Rds")
+source("scripts/functions.R")
+scRNA <- readRDS("data/scRNA/ureter-scRNA.Rds")
 
 #----- Load data
 
 # Since SPOTlight uses the same format as Seurat, we can repurpose the methods to load
-U1.SL <- preProcessSeuratVisium("data/U1", normalization = "LogNormalize")
 U2.SL <- preProcessSeuratVisium("data/U2", normalization = "LogNormalize")
 
 # You may need to generate your own markers. This will take much longer if you do we give the option of loading some from a CSV file for the sample data.
@@ -24,15 +25,11 @@ U2.SL <- preProcessSeuratVisium("data/U2", normalization = "LogNormalize")
 Idents(scRNA) <- scRNA$subclass
 markers <- FindAllMarkers(scRNA, group.by = "subclass")
 
-U1.SL <- spotlightDeconvolve(U1.SL, scRNA, markers)
-U2.SL <- spotlightDeconvolve(U2.SL, scRNA, markers)
+#U2.SL <- spotlightDeconvolve(U2.SL, scRNA, markers)
 
-cell.names <- names(which(colSums(U1.SL@meta.data[,-c(1:4, ncol(U1.SL@meta.data))]) > 0))
-ggsave(plot =  patchwork::wrap_plots(purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U1.SL,
-    cell_types_all = cell.names,
-    cell_types_interest=x,
-    img_path = "data/U1/spatial/tissue_lowres_image.png",
-    pie_scale = 0.4)), ncol = 4), filename="figures/SPOTlight/Figure_3a.pdf", device="pdf", width = 25, height = 25, units = "in", dpi = 300)
+genes<-read.csv(file ="data/scRNA/genes.csv")[,1]
+U2.SL <- spotlightDeconvolve(vis = U2.SL, scrna = scRNA,markers =  markers,hvg=genes)
+
 
 cell.names <- names(which(colSums(U2.SL@meta.data[,-c(1:4, ncol(U2.SL@meta.data))]) > 0))
 ggsave(plot =  patchwork::wrap_plots(purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U2.SL,
@@ -44,19 +41,6 @@ ggsave(plot =  patchwork::wrap_plots(purrr::map(cell.names, function(x) SPOTligh
 
 # OPTIONAL: Added another version of plotting function to print larger fonts in the figures
                                      
-cell.names <- names(which(colSums(U1.SL@meta.data[,-c(1:4, ncol(U1.SL@meta.data))]) > 0))
-scplots<-purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U1.SL,  cell_types_all = cell.names,  cell_types_interest=x,  
-img_path = "data/U1/spatial/tissue_lowres_image.png",
-pie_scale = 0.4)+
- theme(title = element_text(size=20),
-       legend.text = element_text(size = 18),
-       legend.title = element_text(size = 18),
-       axis.title = element_text(size=18),
-       axis.title.y = element_text(angle = 90),
-       axis.text = element_text(size=18)))
-
-patchwork::wrap_plots(scplots, ncol=3) %T>% ggsave(filename="figures/SPOTlight/Figure3a.pdf", device="pdf", width = 25, height = 30, units = "in", dpi = 300)
-
 
 cell.names <- names(which(colSums(U2.SL@meta.data[,-c(1:4, ncol(U2.SL@meta.data))]) > 0))
 scplots<-purrr::map(cell.names, function(x) SPOTlight::spatial_scatterpie(se_obj = U2.SL,  cell_types_all = cell.names,  cell_types_interest=x,  
