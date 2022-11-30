54bb32ac85e3073324a8e960e6ba4b114d46385c
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-11-30 15:06:33 -0500

    Update Giotto.R

--- scripts/Giotto.R
+++ scripts/Giotto.R
@@ -14,42 +14,21 @@ scRNA <- readRDS("data/scRNA/ureter-scRNA.Rds")
 
 #----- Configure workspace with Giotto
 
-# Ensure the Python environment for Giotto has been installed.
-library(Gitoto)
-genv_exists = checkGiottoEnvironment()
-if(!genv_exists){
-  # The following command need only be run once to install the Giotto environment.
-  installGiottoEnvironment()
-}
-
 results_folder = paste0(mydir,'figures/Giotto')
-
-# Install python dependencies for Giotto - not needed if correct Giotto is installed as these will be installed automatically
-#installGiottoEnvironment(packages_to_install = c("pandas==1.1.5", "networkx==2.6.3", "python-igraph==0.9.6", "leidenalg==0.8.7",
-#                                                   "python-louvain==0.15", "scikit-learn==0.24.2"), force_environment = TRUE)
 
 instrs = createGiottoInstructions(save_dir = results_folder,
                                   save_plot = TRUE,
                                   show_plot = FALSE)
 
-my_python_path = NULL
+#my_python_path = NULL
 
 ## Create new Giotto objects for each set of Visium data. We have performed the adjustment necessary, however new sets of Visium data will need to adjust the min_adj and max_adj values as necessary.
 # For more details, please see the Giotto vignette. 
-
-U1 <- createGiottoVisiumObject(visium_dir = 'data/U1', expr_data = 'filter', 
-                              h5_visium_path = 'data/U1/filtered_feature_bc_matrix.h5',
-                              h5_tissue_positions_path = 'data/U1/spatial/tissue_positions_list.csv', 
-                              h5_image_png_path = 'data/U1/spatial/tissue_lowres_image.png',
-                              gene_column_index = 2, instructions = instrs,
-                              h5_json_scalefactors_path= 'data/U2/spatial/scalefactors_json.json', 
-                              xmax_adj = 3200, ymin_adj = 2600, ymax_adj = 2900, xmin_adj = 1600)
 
 U2 <-  createGiottoVisiumObject(visium_dir = 'data/U2', expr_data = 'filter', 
                                      h5_visium_path = 'data/U2/filtered_feature_bc_matrix.h5',
                                      h5_tissue_positions_path = 'data/U2/spatial/tissue_positions_list.csv', 
                                      h5_image_png_path = 'data/U2/spatial/tissue_lowres_image.png', 
-                                     h5_json_scalefactors_path= 'data/U2/spatial/scalefactors_json.json',
                                      gene_column_index = 2, instructions = instrs, xmax_adj = 2000, ymin_adj = 1500, ymax_adj = 1600, xmin_adj = 1400
                                       )
 
@@ -60,7 +39,6 @@ U2 <-  createGiottoVisiumObject(visium_dir = 'data/U2', expr_data = 'filter',
 # spatPlot(gobject = U2, cell_color = 'in_tissue', show_image = T, point_alpha = 0.7,
 #          save_param = list(save_name = 'U2_spatplot_image'))
 
-U1.Giotto<- preProcessGiotto(U1, "U1")
 U2.Giotto<-preProcessGiotto(U2, "U2")
 
 # First you have to create the rank method signature matrix.
@@ -73,12 +51,6 @@ sc_sign_matrix <- makeSignMatrixRank(
 
 
 # Run spatial enrichment for both samples
-U1.Giotto <- runSpatialEnrich(
-  U1.Giotto,
-  enrich_method = c("rank"),
-  sign_matrix = sc_sign_matrix,
-  expression_values = c("normalized"),
-)
 
 U2.Giotto <- runSpatialEnrich(
   U2.Giotto,
@@ -88,24 +60,10 @@ U2.Giotto <- runSpatialEnrich(
 )
 
 # Plot single cell enrichment patterns and simultaneously save figures in folder.
-scplots <- purrr::map(levels(as.factor(scRNA$subclass)), function(i) spatPlot(gobject = U1.Giotto, cell_color=unlist(c(U1@spatial_enrichment$cell$rna$rank[,..i])), point_size = 2) + ggtitle(i) + scale_fill_distiller(palette = "Spectral"))
-patchwork::wrap_plots(scplots, ncol=4) %T>% ggsave(filename = "figures/Giotto/Figure_2a.pdf", width = 25, height = 20, units = "in", dpi = 300)
-
-scplots <- purrr::map(levels(as.factor(scRNA$subclass)), function(i) spatPlot(gobject = U2.Giotto, cell_color=unlist(c(U2@spatial_enrichment$cell$rna$rank[,..i])), point_size = 2) + ggtitle(i) + scale_fill_distiller(palette = "Spectral"))
+scplots <- purrr::map(levels(as.factor(scRNA$subclass)), function(i) spatPlot(gobject = U2.Giotto, cell_color=unlist(c(U2@spatial_enrichment$rank[,..i])), point_size = 2) + ggtitle(i) + scale_fill_distiller(palette = "Spectral"))
 patchwork::wrap_plots(scplots, ncol=4)%T>% ggsave(filename = "figures/Giotto/Figure_2b.pdf", width = 25, height = 20, units = "in", dpi = 300)
                       
 # OPTIONAL: Use the following code for printing larger fonts for figures
-
-scplots <- purrr::map(levels(as.factor(scRNA$subclass)), function(i) spatPlot(gobject = U1.Giotto, cell_color=unlist(c(U1.Giotto@spatial_enrichment$cell$rna$rank[,..i])), point_size = 2) + 
-theme(title = element_text(size=18),
-          legend.text = element_text(size = 15),
-          legend.title = element_text(size = 15),
-          axis.title = element_text(size=15),
-          axis.text = element_text(size=15)) + 
-ggtitle(i) + scale_fill_distiller(palette = "Spectral"))
-
-patchwork::wrap_plots(scplots, ncol=4) %T>% ggsave(filename = "figures/Giotto/Figure_2a.pdf", width = 25, height = 20, units = "in", dpi = 300)
-
                       
 scplots <- purrr::map(levels(as.factor(scRNA$subclass)), function(i) spatPlot(gobject = U2.Giotto, cell_color=unlist(c(U2.Giotto@spatial_enrichment$cell$rna$rank[,..i])), point_size = 2) + 
 theme(title = element_text(size=18),
